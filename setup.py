94a9d603001f174f92d9fa89ff4c1465ca01ea01
Stephen Newell <snewell4@.gmail.com>
2022-06-06 12:04:15 -0500

    Add requirements.txt to RtD yaml file - This should work. My last update was a goof.
    setup.py
    - New version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simpleth',
-    version='0.1.13',
+    version='0.1.14',
     author='Stephen Newell',
     author_email='<snewell4@gmail.com>',
     description='Simplified Ethereum for Python',
