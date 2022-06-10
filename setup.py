ae8cfe65164d0d2047cf728d05f9c46cf43288b0
Stephen Newell <snewell4@.gmail.com>
2022-06-10 18:42:35 -0500

    Installing doc update for readability
    setup.py
    - new version v0.1.32
    
    installing.rst
    - Edit for readability
    
    CHANGES.txt
    - new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='simpleth',
-    version='0.1.31',
+    version='0.1.32',
     author='Stephen Newell',
     author_email='<snewell4@gmail.com>',
     description='Simplified Ethereum for Python',
