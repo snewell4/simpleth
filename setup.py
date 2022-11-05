ea53c8bd95dff7276d3cebea1251b52e929cb748
Stephen Newell <snewell4@.gmail.com>
2022-11-05 17:50:56 -0500

    Add section to explain using bytes data and fix missing artifacts
    Artifacts
    - Added latest artifacts for Test.sol
    - There was a problem that my compiles were not going there.
    - Error started in last release. This fixes it.
    
    V0.1.58
    - Updated in CHANGES.txt
    - Updated in conf.py
    - Updated in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='simpleth',
-    version='0.1.57',
+    version='0.1.58',
     author='Stephen Newell',
     author_email='<snewell4@gmail.com>',
     description='Simplified Ethereum for Python',
